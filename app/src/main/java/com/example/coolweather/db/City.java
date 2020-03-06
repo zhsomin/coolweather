75826730ead285f18d3e7970d171b5dcaabf51c4
somin <achouloves@163.com>
2020-03-06 22:08:50 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/example/coolweather/db/City.java
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -3,31 +3,31 @@ package com.example.coolweather.db;
 import org.litepal.crud.DataSupport;
 
 public class City extends DataSupport {
-    private  String cityNmae;
-    private  int  cityCode;
-    private  int  provinceId;
-
-
-    public String getCityNmae() {
-        return cityNmae;
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+    public int getId() {
+        return id;
     }
-
-    public void setCityNmae(String cityNmae) {
-        this.cityNmae = cityNmae;
+    public void setId(int id) {
+        this.id = id;
     }
-
+    public String getCityName() {
+        return cityName;
+    }
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
     public int getCityCode() {
         return cityCode;
     }
-
     public void setCityCode(int cityCode) {
         this.cityCode = cityCode;
     }
-
     public int getProvinceId() {
         return provinceId;
     }
-
     public void setProvinceId(int provinceId) {
         this.provinceId = provinceId;
     }
